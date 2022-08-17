706bb0a3b40ed65b2f52daef0dbd878b7afc2ee3
yeetnbeet <segalcsam@gmail.com>
2022-08-17 16:47:43 -0600

    Todo finish the input phase

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ key = os.getenv("SECRET_TOKEN")
 URLForCollection = "https://sams-test-store-app.myshopify.com/admin/api/2022-10/collections/291616489658/products.json?"
 H = {"X-Shopify-Access-Token":key}
 
+#input the collection ID and it gives a list of product IDs
 def getProductsFromCollection(collectionID):
     URLForCollection = "https://sams-test-store-app.myshopify.com/admin/api/2022-10/collections/"+str(collectionID)+"/products.json?" #TODO eventually add collection ID
     res = requests.get(URLForCollection,headers=H)
@@ -25,7 +26,7 @@ def getProductsFromCollection(collectionID):
 
     return idList
 
-#TODO the first item returned in inventory list has no sku?
+#input the list of Product IDs and it outputs a list of SKU and Inventory id for each variant
 def getSKU_Inventory(idList):
     inventorylist = []
     counter = 0
@@ -33,19 +34,33 @@ def getSKU_Inventory(idList):
         URLForCollection = "https://sams-test-store-app.myshopify.com/admin/api/2022-10/products/"+str(id)+"/variants.json"
         res = requests.get(URLForCollection,headers=H)
         resdata = res.json()
-        time.sleep(1)
+        time.sleep(.3)
         counter += 1
         for item in resdata["variants"]:
             print("SKU: ",item['sku'],"\n")
             print(item['inventory_item_id']," quant:",item['inventory_quantity'],"\n-------------------")
             inventorylist.append([item["sku"],item["inventory_item_id"]])
     
-    
-    
     return inventorylist
+#input the list of SKU and Inventory and It will output a matrix of [Sku,InventoryId,inventoryID1,inventoryID2,inventoryID3...] 
+def getInventoryLevel(inventoryList):
+    locations = [] #[[locationname1,locationID1],[locationname2,locationID2]]
+    res = requests.get("https://sams-test-store-app.myshopify.com/admin/api/2022-10/locations.json",headers=H)
+    resdata = res.json()
+
+    for line in resdata["locations"]:
+        locations.append(line["id"])
+
+    for line in inventoryList:
+        #TODO iterate thru the SKU and inventory ID and fill the matrix
+        print(line)
+
+    return locations
 
 idlist=getProductsFromCollection(291616489658)
 framesList = getSKU_Inventory(idlist)
+framesList.pop(0)
+print(getInventoryLevel(framesList))
 for i in framesList:
     print(i)
 
